cf74ba6ba81e411e9ea2321b32172a109b221459
Alexander Maslov <drakmail@gmail.com>
2014-10-29 12:31:14 +0300

    Add constants example file

---
+++ spec/support/constants.example.rb
@@ -0,0 +1,13 @@
+# Shared constants for specs
+
+ACCESS_TOKEN = ""
+CLIENT_ID = ""
+REDIRECT_URI = "http://127.0.0.1:4567/redirect"
+INSTANCE_ID = ""
+
+# Just wallet number for test
+WALLET_NUMBER = ""
+# Any operation from this wallet
+OPERATION_ID  = ""
+# Payment request ID
+REQUEST_ID = ""
