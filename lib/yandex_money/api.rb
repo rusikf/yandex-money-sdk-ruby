ce4ad63fb18cdaf8ac34184880dff68b0cdc0141
Alexander Maslov <drakmail@gmail.com>
2015-01-31 11:46:04 +0300

    Remove unused logger parts

--- lib/yandex_money/api.rb
+++ lib/yandex_money/api.rb
@@ -1,7 +1,5 @@
 require "yandex_money/api/version"
 require "yandex_money/config"
 require "yandex_money/exceptions"
-require "yandex_money/logger/default"
-require "yandex_money/logger/empty"
 require "yandex_money/wallet"
 require "yandex_money/external_payment"
